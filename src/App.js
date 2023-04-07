d68ca94535c694aff2baff3e6231dfd8e41b313b
clarsongit <clarson@berkeley.edu>
2023-04-07 12:28:44 -0700

    Imported logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import logo from "./cl-logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="img-fluid App-logo" alt="cl logo" />
       </header>
     </div>
   );
