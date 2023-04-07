714fbbd72fd9cc1204196cb1fe1967aaa74b6616
clarsongit <clarson@berkeley.edu>
2023-04-07 13:00:57 -0700

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./cl-logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="img-fluid App-logo" alt="cl logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="img-fluid App-logo" alt="cl logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This project was coded by Chloe Larson and is open-sourced on Github.
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    return null;
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <input type="submit" value="search" />
+      </form>
+    </div>
+  );
+}
