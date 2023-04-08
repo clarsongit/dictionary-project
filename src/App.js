557c9ef8e5028e9825bd65cc6063c4e85812b8ad
clarsongit <clarson@berkeley.edu>
2023-04-08 11:44:05 -0700

    Added footer links

--- src/App.js
+++ src/App.js
@@ -13,7 +13,22 @@ function App() {
           <Dictionary />
         </main>
         <footer>
-          This project was coded by Chloe Larson and is open-sourced on Github.
+          This project was coded by{" "}
+          <a
+            href="https://clarson.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Chloe Larson
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/clarsongit/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github.
+          </a>
         </footer>
       </div>
     </div>
