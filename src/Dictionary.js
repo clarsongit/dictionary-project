f49ec5bd99a77987cfc7fa7e8b03a9708bbbaae1
clarsongit <clarson@berkeley.edu>
2023-04-13 12:40:07 -0700

    Added image API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,16 +8,24 @@ export default function Dictionary(props) {
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     console.log(response.data[0]);
     console.log(response.data[0].meanings[0].definitions[0].definition);
     setResults(response.data[0]);
   }
 
+  function handleImagesResponse(response) {
+    console.log(response);
+  }
+
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let imagesApiKey = "018f3ce3643803cb4575b08a7ct1f8o9";
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
 
   function handleSubmit(event) {
