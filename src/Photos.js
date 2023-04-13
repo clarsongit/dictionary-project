8bb1151a494841804bade56dbc8af28c8abefad4
clarsongit <clarson@berkeley.edu>
2023-04-13 13:43:35 -0700

    Fixed link error message

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="norefferer">
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener norefferer"
+                >
                   <img src={photo.src.landscape} className="img-fluid" />
                 </a>
               </div>
