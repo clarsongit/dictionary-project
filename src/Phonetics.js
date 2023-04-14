70a2614484fa2cbbed2cbf05bd70440d59a487a5
clarsongit <clarson@berkeley.edu>
2023-04-14 14:00:06 -0700

    Adjusted spacing for two listen buttons

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  if (props.phonetics.audio) {
+  if (props.phonetics.audio && props.phonetics.text) {
     return (
       <div className="Phonetics">
         <a
